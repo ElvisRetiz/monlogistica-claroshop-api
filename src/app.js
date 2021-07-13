86986c2b2e3574634b8c036a10fbb52b3a0036f5
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-13 16:34:36 -0500

    set app file

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+//CORS Configuration
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+//Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Routes
+
+module.exports = app;
