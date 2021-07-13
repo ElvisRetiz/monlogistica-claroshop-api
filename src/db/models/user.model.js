c3041056e94dddc1d381b4bd21b257990198e2ff
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-13 17:31:18 -0500

    create and set the user model file

---
+++ src/db/models/user.model.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../index');
+
+class User extends Model {};
+
+User.init({
+    usuario: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    password: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    correo: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    tipo: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    activo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    },
+    dentro: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    }, 
+    ultimoAcceso: {
+        type: DataTypes.DATE,
+        allowNull: true
+    }
+},{
+    sequelize,
+    modelName: 'User',
+    tableName: 'Usuarios',
+    timestamps: false
+})
