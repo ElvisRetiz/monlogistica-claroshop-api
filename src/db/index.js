e408bee7202ded9dd8d58ab09e0a4f7e9caebd74
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-15 13:17:12 -0500

    Corrects connection problem in local database

--- src/db/index.js
+++ src/db/index.js
@@ -2,16 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-let dbUser = "sa", dbPassword = "kitsacv", dbServer = 'ELVIS-RETIZW', dbName = "ClaroShop"
+let dbUser = "sa", dbPassword = "kitsacv", dbServer = 'ELVIS-RETIZ', dbName = "ClaroShop"
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbServer,
-  dialect: 'mssql',
-  dialectOptions: { 
+  dialect: 'mssql'
+  /*dialectOptions: { 
     options: {
       instanceName: 'SQLEXPRESS'
     }
-  }
+  }*/
 });
 
 module.exports = sequelize;
