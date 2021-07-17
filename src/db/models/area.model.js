0b8b598998380abb313a8658956b896c7ea0acde
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-17 12:21:03 -0500

    Create the model of the areas table

---
+++ src/db/models/area.model.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../index');
+
+class Area extends Model {};
+
+Area.init({
+    area: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+        primaryKey: true
+    },
+    descripcion: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    activo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    }
+},{
+    sequelize,
+    modelName: 'Area',
+    tableName: 'Areas',
+    timestamps: false
+})
+
+module.exports = Area;
