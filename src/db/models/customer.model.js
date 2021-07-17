3d17b35e9fc7ba7f71e48a9b88f467661fed3cce
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-17 00:02:24 -0500

    Create the model of the Customers table

---
+++ src/db/models/customer.model.js
@@ -0,0 +1,71 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../index');
+
+class Customer extends Model {};
+
+Customer.init({
+    cliente: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    razonSocial: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    direccion: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    numeroExt: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    numeroInt: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    colonia: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    ciudad: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    estado: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    codigoPostal: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    rfc: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    contacto: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    conTelefono: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    conEmail: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+    activo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+    }
+},{
+    sequelize,
+    modelName: 'Customer',
+    tableName: 'Clientes',
+    timestamps: false
+});
+
+module.exports = Customer;
