bc3725a8538793af39da4eb93ceda1eca3870a1b
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-16 23:43:39 -0500

    Add routing to bring in a branch and create a branch

--- src/routes/branch.route.js
+++ src/routes/branch.route.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/branch.controller');
 const router = express.Router();
 
 router.get("/",controller.getBranches);
+router.get("/:sucursal", controller.getBranch);
+router.post("/",controller.createBranch);
 
 module.exports = router;
