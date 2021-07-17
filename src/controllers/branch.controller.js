9d175f197850a401db6273b20cf3f30ea6be16a0
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-16 23:21:43 -0500

    Manage a branch query

--- src/controllers/branch.controller.js
+++ src/controllers/branch.controller.js
@@ -14,6 +14,21 @@ const controller = {
                 message: error.message
             })
         }
+    },
+    getBranch: async (req, res) => {
+        try {
+            const { sucursal } = req.params;
+            let branch = await Branch.findOne({
+                where: { sucursal: sucursal}
+            })
+            res.send(branch);
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            })
+        }
     }
 };
 
