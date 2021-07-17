8e9d8d7e00746990d9c03494c89fc67c021a7a4b
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-16 23:40:46 -0500

    Manage the registration of a new branch

--- src/controllers/branch.controller.js
+++ src/controllers/branch.controller.js
@@ -29,6 +29,38 @@ const controller = {
                 message: error.message
             })
         }
+    },
+    createBranch: async (req, res) => {
+        try {
+            const { 
+                cliente,
+                sucursal,
+                descripcion,
+                direccion,
+                numeroExt,
+                numeroInt,
+                colonia,
+                ciudad,
+                estado,
+                codigoPostal,
+                contacto,
+                conTelefono,
+                conEmail
+             } = req.body;
+
+             await sequelize.query(`EXEC sp_SucursalesInsertar ${cliente},'${sucursal}','${descripcion}','${direccion}','${numeroExt}','${numero}','${colonia}','${ciudad}','${estado}','${codigoPostal}','${contacto}','${conTelefono}','${conEmail}', 1`);
+             
+             res.send({
+                type: "Success",
+                message: "La sucursal fue registrada satisfactoriamente"
+            });
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
     }
 };
 
