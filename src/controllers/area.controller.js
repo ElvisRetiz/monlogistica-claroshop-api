f043c2ca253f6ef131e994bc3d8e7746f5a46e78
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-17 12:32:05 -0500

    Create the areas controller

---
+++ src/controllers/area.controller.js
@@ -0,0 +1,65 @@
+const sequelize = require('../db/index');
+
+const Area = require('../db/models/area.model');
+
+const controller = {
+    getAreas: async (req, res) => {
+        try {
+            let areas = await Area.findAll();
+            return res.send(areas);
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    },
+    getArea: async (req, res) => {
+        try {
+            const { area } = req.params;
+            let area = await Area.findByPk(area);
+            return res.send(area);
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    },
+    createArea: async (req, res) => {
+        try {
+            const { area, descripcion } = req.body;
+            await sequelize.query(`EXEC sp_AreasInsertar '${area}', '${descripcion}', 1`);
+            return res.send({
+                type: "Success",
+                message: "Area fue creada satisfactoriamente"
+            });
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    },
+    deleteArea: async (req, res) => {
+        try {
+            const { area } = req.query;
+            await sequelize.query(`EXEC sp_AreasBorrar '${area}'`);
+            return res.send({
+                type: "Success",
+                message: "Area eliminada satisfactoriamente"
+            });
+        } catch (error) {
+            console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    }
+};
+
+module.exports = controller;
