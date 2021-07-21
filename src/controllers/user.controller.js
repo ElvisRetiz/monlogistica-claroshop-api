04d8d8a135c5a189a45c415ac38dded2a29e7ca5
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-21 17:24:17 -0500

    Fix the error with the date format

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
-const Sequelize = require('sequelize');
+const bcrypt = require('bcrypt');
+const dayjs = require('dayjs');
 const sequelize = require('../db/index');
 
 const User = require('../db/models/user.model');
@@ -13,27 +14,54 @@ const controller = {
             return  res.send({
                 type: "Error",
                 message: error.message
-            })
+            });
         }
     },
     getUser: async (req,res) => {
         try {
             const { usuario } = req.params;
-            let user = await User.findByPk(usuario)
+            let user = await User.findByPk(usuario);
             return res.send(user);
         } catch (error) {
             console.log(error);
             return  res.send({
                 type: "Error",
                 message: error.message
-            })
+            });
         }
     },
     createUser: async (req, res) => {
         try {
             const { usuario, nombre, password, correo, tipo } = req.body;
-
-            await sequelize.query(`EXEC sp_UsuariosInsertar '${usuario}','${nombre}','${password}','${correo}',${tipo},0,NULL,1`);
+            let user = await User.findByPk(usuario);
+            if (!user) {
+                let hashPassword = await bcrypt.hash(password,5);
+                await sequelize.query(`EXEC sp_UsuariosInsertar '${usuario}','${nombre}','${hashPassword}','${correo}',${tipo},0,NULL,1`);
+            } else {
+                console.log("PASSWORD: ", !password);
+                let hashPassword = user.password;
+                let userName = user.nombre;
+                let userMail = user.correo;
+                let userType = user.tipo;
+                let userLastConnection = user.ultimoAcceso;
+                if (password) {
+                    hashPassword = await bcrypt.hash(password,5);
+                };
+                if (nombre) {
+                    userName = nombre;
+                };
+                if (correo) {
+                    userMail = correo;
+                };
+                if (tipo) {
+                    userType = tipo;
+                };
+                if (userLastConnection) {
+                    user.ultimoAcceso.setDate(user.ultimoAcceso.getDate()+1);
+                    userLastConnection = `'${dayjs(user.ultimoAcceso).format('YYYYMMDD')}'`;
+                };
+                await sequelize.query(`EXEC sp_UsuariosInsertar '${usuario}','${userName}','${hashPassword}','${userMail}',${userType},${user.dentro === true ? 1 : 0},${userLastConnection},${user.activo === true ? 1 : 0}`);
+            }
             return res.send({
                 type: "Success",
                 message: "El usuario fue creado satisfactoriamente"
@@ -43,7 +71,7 @@ const controller = {
             return  res.send({
                 type: "Error",
                 message: error.message
-            })
+            });
         }
     },
     deleteUser: async (req,res) => {
@@ -53,8 +81,7 @@ const controller = {
             return res.send({
                 type: "Success",
                 message: "El usuario fue eliminado satisfactoriamente"
-            })
-
+            });
         } catch (error) {
             console.log(error);
             return  res.send({
