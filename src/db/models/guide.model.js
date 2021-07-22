5a0e974022e6a7ddc580366663f5a724828f5042
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-22 07:57:50 -0500

    Add the new fields to the model of the guides

--- src/db/models/guide.model.js
+++ src/db/models/guide.model.js
@@ -17,6 +17,74 @@ Guide.init({
     estatus: {
         type: DataTypes.CHAR,
         allowNull: true
+    },
+    fechaRecoleccion: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+	nombreClienteFinal: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	direccion: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	numeroExt: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	numeroInt: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	colonia: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	ciudad: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	estado: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	codigoPostal: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	fechaEntregaAlmacen: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+	choferEntrega: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	FechaEntregaChofer: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+	RecibeFecha: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+	RecibePersona: {
+        type: DataTypes.CHAR,
+        allowNull: true
+    },
+	fotoIfe: {
+        type: DataTypes.BLOB,
+        allowNull: true
+    },
+	fotoCasa: {
+        type: DataTypes.BLOB,
+        allowNull: true
+    },
+	devueltaAlmacen: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
     }
 },{
     sequelize,
