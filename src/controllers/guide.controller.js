2a5bc474a63b41c2e5c38f1f3cea1280d6cc910c
Elvis Retiz <elvis.retiz@hotmail.com>
2021-07-22 14:35:43 -0500

    Add the handling of guidance states

--- src/controllers/guide.controller.js
+++ src/controllers/guide.controller.js
@@ -1,3 +1,4 @@
+const dayjs = require('dayjs');
 const sequelize = require('../db/index');
 
 const Guide = require('../db/models/guide.model');
@@ -24,6 +25,40 @@ const controller = {
             return res.send(guides)
         } catch (error) {
             console.log(error);
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    },
+    markGuidesAsCollected: async (req, res) => {
+        try {
+            const { orden, guia, fecha }  = req.body;
+            let guideDate = new Date(fecha);
+            guideDate.setDate(guideDate.getDate()+1);
+            await sequelize.query(`EXEC sp_RecoleccionGuiasInsertar ${orden},'${guia}','${dayjs(guideDate).format('YYYYMMDD')}'`);
+            return  res.send({
+                type: "Succes",
+                message: "Las guias fueron marrcadas como recolectadas con exito"
+            });
+        } catch (error) {
+            return  res.send({
+                type: "Error",
+                message: error.message
+            });
+        }
+    },
+    markGuideAsDelivered: async (req, res) => {
+        try {
+            const { orden, guia, fecha, recibePersona, fotoIfe, fotoCasa }  = req.body;
+            let guideDate = new Date(fecha);
+            guideDate.setDate(guideDate.getDate()+1);
+            await sequelize.query(`EXEC sp_RecoleccionGuiasEntrega ${orden},'${guia}','${dayjs(guideDate).format('YYYYMMDD')}','${recibePersona}','${fotoIfe}','${fotoCasa}'`);
+            return  res.send({
+                type: "Succes",
+                message: "La guia fue marrcada como entregada con exito"
+            });
+        } catch (error) {
             return  res.send({
                 type: "Error",
                 message: error.message
